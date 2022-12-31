059829d20537c9fecd9eaec4eb06b4742c34c63e
Anh Du Cong Hoang <hoanganh.du@ascension6.com.au>
2022-12-31 21:48:48 +0700

    Revert "customized commit message"
    This reverts commit 263bace1c6208f248cd5effb7abf2b661ff4c2b9.

--- django-social-media-website/social_book/settings.py
+++ django-social-media-website/social_book/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-y6on4&^y1+^c$5_h4eh-i2fz-c6jmagos9$5b!o!%8=08s#!tm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
